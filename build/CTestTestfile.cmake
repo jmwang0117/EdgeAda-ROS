af0292350741e96bbf1e7223efed087adb030bcf
jmwang0117 <1021347250@qq.com>
2023-11-01 05:13:51 +0000

    Initial

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /root/Edge_Adapter/src
+# Build directory: /root/Edge_Adapter/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("perception")
---
+++ build/perception/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/Edge_Adapter/src/perception
+# Build directory: /root/Edge_Adapter/build/perception
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
