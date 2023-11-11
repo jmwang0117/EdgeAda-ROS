f6a9e25ecfeaa416e0b06aef1343bb04accdd814
Junming WANG <51500826+jmwang0117@users.noreply.github.com>
2023-11-12 01:03:13 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/Edge_Adapter/src
-# Build directory: /root/Edge_Adapter/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("perception")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/root/Edge_Adapter/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/root/cv_bridge_ws/devel_isolated/vision_opencv;/root/cv_bridge_ws/devel_isolated/opencv_tests;/root/cv_bridge_ws/devel_isolated/image_geometry;/root/cv_bridge_ws/devel_isolated/cv_bridge;/opt/ros/melodic'.split(';') if '/root/cv_bridge_ws/devel_isolated/vision_opencv;/root/cv_bridge_ws/devel_isolated/opencv_tests;/root/cv_bridge_ws/devel_isolated/image_geometry;/root/cv_bridge_ws/devel_isolated/cv_bridge;/opt/ros/melodic' != '' else []
